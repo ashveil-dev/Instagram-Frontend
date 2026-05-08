c3388a7ef78be37de7dd543e8a80ad0b706f0b3f
ashveil <ashveil.dev@gmail.com>
2026-05-09 08:12:51 +0900

    @Set 사용하지 않는 서버 IP를 로컬 호스트로 변경하기

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
+// http://vitejs.dev/config/
 export default defineConfig({
 	server: {
 		port: 4173,
 		host: "0.0.0.0",
 		proxy : {
 			"/api" : {
-				target : "https://52.63.35.30:4000/instagram",
+				target : "http://localhost:4000/",
 				changeOrigin : true,
-				rewrite : (path) => path.replace(/^\/api/, ''),
 				secure: false,
 				ws : true
 			}
